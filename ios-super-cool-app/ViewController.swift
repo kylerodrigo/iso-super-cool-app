8dae64d394d9fe06be9215876a9d615f58c96a02
Kyle P. Rodrigo <kiprodrigo@gmail.com>
2016-02-21 16:58:02 -0500

    Initial Commit

---
+++ ios-super-cool-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-super-cool-app
+//
+//  Created by Kyle Rodrigo on 2/21/16.
+//  Copyright © 2016 Kyle Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
